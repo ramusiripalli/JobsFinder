a489c9ae32d4d1864e9832e998244c2042b4da03
ramu siripalli <ramsiripalli2772@gmail.com>
2025-04-12 12:42:54 +0530

    Working Code: Landing Page, Login , Registration and Dashboard

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ mongoose.connect(process.env.MONGO)
 app.use("/api/auth", authRoutes);
 app.use('/api/jobs', jobRoutes);
 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
 
